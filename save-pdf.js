ae0faf0eb3bc62520b9000fd616291b3ffa98b76
Tim Finnigan <tim.finnigan1@gmail.com>
2020-04-24 09:01:24 -0400

    add script for saving pdfs

---
+++ save-pdf.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+
+  await page.goto('https://news.google.com', {waitUntil: 'networkidle2'});
+
+  await page.pdf({path: 'news.pdf', format: 'A4'});
+
+  await browser.close();
+
+})();
