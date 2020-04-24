cda3feaa9f702ac17b9e274b51e96283426f1306
Tim Finnigan <tim.finnigan1@gmail.com>
2020-04-24 12:02:24 -0400

    add script to fetch youtube categories

---
+++ fetch-youtube-categories.js
@@ -0,0 +1,26 @@
+// Fetch data using YouTube API
+const fetch = require("node-fetch");
+const fs = require("fs");
+const config = require("./config.js");
+
+const youtubeKey = config.YOUTUBE_API_KEY;
+
+let url =
+  "https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=";
+
+url += youtubeKey;
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    fs.writeFile(
+      "youtube-categories-data.json",
+      JSON.stringify(data),
+      function (err) {
+        if (err) throw err;
+      }
+    );
+    console.log("Saved JSON file");
+  });
